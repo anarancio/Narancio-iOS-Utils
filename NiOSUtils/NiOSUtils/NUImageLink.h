bd462e1238c64dc80029e1f346c5d381449b8a68
Alejandro Narancio <ale.narancio@gmail.com>
2012-03-01 15:27:14 -0200

    NUImageLink base

---
+++ NiOSUtils/NiOSUtils/NUImageLink.h
@@ -0,0 +1,13 @@
+//
+//  NUImageLink.h
+//  NiOSUtils
+//
+//  Created by Alejandro Narancio on 3/1/12.
+//  Copyright (c) 2012 Infuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NUImageLink : UIImageView
+
+@end
---
+++ NiOSUtils/NiOSUtils/NUImageLink.m
@@ -0,0 +1,13 @@
+//
+//  NUImageLink.m
+//  NiOSUtils
+//
+//  Created by Alejandro Narancio on 3/1/12.
+//  Copyright (c) 2012 Infuy. All rights reserved.
+//
+
+#import "NUImageLink.h"
+
+@implementation NUImageLink
+
+@end
